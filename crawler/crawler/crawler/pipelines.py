6542c0c26f2c478365a3a8fe2554add70169eadc
Anagha Dudihalli <anaghadudihalli@gmail.com>
2020-04-29 09:37:33 -0500

    Add crawler

---
+++ crawler/crawler/crawler/pipelines.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
+
+class CrawlerPipeline(object):
+    count = 0
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect('pages')
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""CREATE TABLE IF NOT EXISTS pages(
+                        title text,
+                        url text,
+                        content text
+                        )""")
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+        return item
+
+    def store_db(self, item):
+        self.curr.execute("""insert into pages values(?,?,?)""", (
+            item['title'],
+            item['url'],
+            item['content']
+        ))
+        self.conn.commit()
