77d24127630bff448fdbf605ff73619e58f899f2
Anagha Dudihalli <anaghadudihalli@gmail.com>
2020-05-02 11:51:22 -0500

    Remove pipelines flow - saving as json instead of in DB

--- crawler/crawler/crawler/settings.py
+++ crawler/crawler/crawler/settings.py
@@ -63,9 +63,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-    'crawler.pipelines.CrawlerPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#     'crawler.pipelines.CrawlerPipeline': 300,
+# }
 
 CLOSESPIDER_PAGECOUNT = 30
 # Enable and configure the AutoThrottle extension (disabled by default)
