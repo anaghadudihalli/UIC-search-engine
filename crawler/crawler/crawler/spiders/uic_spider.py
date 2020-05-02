aaf3cc5ca9b7afba76ec228b5b49d0fbad64d1a4
Anagha Dudihalli <anaghadudihalli@gmail.com>
2020-05-02 00:30:06 -0500

    Extract visible text from a page

--- crawler/crawler/crawler/spiders/uic_spider.py
+++ crawler/crawler/crawler/spiders/uic_spider.py
@@ -4,6 +4,17 @@ from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 
 from ..items import CrawlerItem
 from scrapy.spiders import CrawlSpider, Rule
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]', 'footer', 'menu', 'img',
+                               'form', 'input', 'noscript', 'svg', 'path']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 
 class UicSpiderSpider(CrawlSpider):
@@ -15,27 +26,33 @@ class UicSpiderSpider(CrawlSpider):
         'DEPTH_PRIORITY': 5,
     }
 
-    rules = [Rule(LxmlLinkExtractor(allow_domains=('uic.edu'), deny_domains=('login.uic.edu')), callback='parse_data',
+    rules = [Rule(LxmlLinkExtractor(allow_domains=('uic.edu'), deny_domains=('login.uic.edu'), canonicalize=True),
+                  callback='parse_data',
                   follow=True)]
 
     def parse_data(self, response):
-        data = ''
         item = CrawlerItem()
         title = response.css("head title::text").extract_first().strip()
+
+        # Extract page title
         if title.endswith(' | University of Illinois at Chicago'):
             title = title[:-36]
-        content = response.css("p::text").extract()
-        print(content)
-        for string in content:
-            if len(string.strip()) > 2 and string.strip() != 'We recommend using the latest version of IE11, Edge, Chrome, Firefox or Safari.' and 'Copyright' not in string.strip():
-                data += string.strip() + ' '
+        soup = BeautifulSoup(response.text, "lxml")
+        for div in soup.find_all("div", {'class': 'browser-stripe'}):
+            div.decompose()
+
+        # Extract page content
+        contents = soup.findAll(text=True)
+        visible_texts = filter(tag_visible, contents)
+        item['content'] = " ".join(t.strip() for t in visible_texts)
+
         outlinks = []
-        le = LxmlLinkExtractor(allow_domains=('uic.edu'), deny_domains=('login.uic.edu'), unique=True)
+        le = LxmlLinkExtractor(allow_domains=('uic.edu'), deny_domains=('login.uic.edu'), unique=True,
+                               canonicalize=True)
         for link in le.extract_links(response):
             outlinks.append(link.url)
 
         item['title'] = title
         item['url'] = response.request.url
-        item['content'] = data
         item['outlinks'] = str(outlinks)
         yield item
