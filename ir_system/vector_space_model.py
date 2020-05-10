a034de225b985f8fc76c6bfbcd8a1bd18c105499
Anagha Dudihalli <anaghadudihalli@gmail.com>
2020-05-10 00:17:38 -0500

    Update vector space model

--- ir_system/vector_space_model.py
+++ ir_system/vector_space_model.py
@@ -1,21 +1,24 @@
 import json
+import os
 import string
 import re
 import math
+import pickle
 
 import nltk
 from nltk.corpus import stopwords
-
-stop_words = list(set(stopwords.words('english')))
 from nltk.stem import PorterStemmer
 
-weight_squares = []
+stop_words = list(set(stopwords.words('english')))
 
 
 def read_files():
     corpus = []
+    cur_dir = os.path.dirname(__file__)
+    relative_path = '../crawler/crawler/4000.json'
+    abs_file_path = os.path.join(cur_dir, relative_path)
 
-    with open('crawler/crawler/data.json') as f:
+    with open(abs_file_path) as f:
         data = json.load(f)
 
     for page in data:
@@ -46,7 +49,6 @@ def tokenize_data(collection):
     """
     result = []
     for document in collection:
-        tokens = []
         tokens = nltk.word_tokenize(document)
         result.append(tokens)
     return result
@@ -61,7 +63,6 @@ def eliminate_stopwords(tokenized_collection):
     global stop_words
     result = []
     for document in tokenized_collection:
-        cleaned_tokens = []
         cleaned_tokens = [word for word in document if word not in stop_words]
         result.append(cleaned_tokens)
     return result
@@ -76,7 +77,6 @@ def stem_tokens(tokenized_collection):
     stemmer = PorterStemmer()
     result = []
     for document in tokenized_collection:
-        stemmed_tokens = []
         stemmed_tokens = [stemmer.stem(word) for word in document]
         result.append(stemmed_tokens)
     return result
@@ -90,7 +90,6 @@ def eliminate_one_two_character_words(collection):
     """
     result = []
     for document in collection:
-        new_words = []
         new_words = [word for word in document if len(word) > 2]
         result.append(new_words)
     return result
@@ -107,13 +106,13 @@ def build_inverted_index(collection):
         for word in document:
             if word not in inverted_index:
                 temp = {}
-                temp[i + 1] = 1
+                temp[i] = 1
                 inverted_index[word] = temp
             else:
-                if i + 1 not in inverted_index[word]:
-                    inverted_index[word][i + 1] = 1
+                if i not in inverted_index[word]:
+                    inverted_index[word][i] = 1
                 else:
-                    inverted_index[word][i + 1] += 1
+                    inverted_index[word][i] += 1
     return inverted_index
 
 
@@ -124,19 +123,19 @@ def cal_document_weights(inverted_index, collection):
         output: document weights
     """
     document_weights = {}
-    global weight_squares
+    weight_squares = []
     for i, document in enumerate(collection):
         temp = {}
         weights_sum = 0
         for word in document:
             if word not in temp:
-                # weight = tf * idf
-                temp[word] = (inverted_index[word][i + 1] / len(document)) * math.log2(
+                temp[word] = (inverted_index[word][i] / len(document)) * math.log2(
                     len(collection) / len(inverted_index[word]))
                 weights_sum += math.pow(temp[word], 2)
         weight_squares.append(weights_sum)
-        document_weights[i + 1] = temp
-    return document_weights
+        document_weights[i] = temp
+    return document_weights, weight_squares
+
 
 def cal_query_weights(query_terms, inverted_index, collection):
     """ Calculates and returns query weights
@@ -146,33 +145,32 @@ def cal_query_weights(query_terms, inverted_index, collection):
         query_token_weights = {}
         for term in query:
             if term in inverted_index:
-                tf = query.count(term)/len(query)
-                idf = math.log2(len(collection)/len(inverted_index[term]))
+                tf = query.count(term) / len(query)
+                idf = math.log2(len(collection) / len(inverted_index[term]))
                 tf_idf = tf * idf
                 query_token_weights[term] = tf_idf
-        query_weights[i+1] = query_token_weights
+        query_weights[i] = query_token_weights
     return query_weights
 
-def cal_cosine_similarity(query_list, collection, document_weights, query_weights):
+
+def cal_cosine_similarity(query_list, collection, document_weights, query_weights, weight_squares_list):
     """ Calculates and returns cosine similarities
     """
-    global weight_squares
     cosine_similarities_list = {}
     for i, query in enumerate(query_list):
         query_doc_cosine_similarities = {}
         for j, document in enumerate(collection):
-            common_terms = list(set(query)&set(document))
+            common_terms = list(set(query) & set(document))
             query_document_weights_sum = 0
             if common_terms:
                 for term in common_terms:
-                    # Numerator
-                    query_document_weights_sum += query_weights[i+1][term] * document_weights[j+1][term]
-                # Cosine similarities = Numerator / Denominator
-                query_doc_cosine_similarities[j+1] = query_document_weights_sum/math.sqrt(weight_squares[j])
-        cosine_similarities_list[i+1] = query_doc_cosine_similarities
+                    query_document_weights_sum += query_weights[i][term] * document_weights[j][term]
+                query_doc_cosine_similarities[j] = query_document_weights_sum / math.sqrt(weight_squares_list[j])
+        cosine_similarities_list[i] = query_doc_cosine_similarities
     return cosine_similarities_list
 
-def sort_cosine_similarities(cosine_similarities):
+
+def sort_cosine_similarities(cosine_similarities, n):
     """Sorts and returns a list of cosine similarities in decreasing order"""
 
     sorted_cosine_similarities = {}
@@ -180,29 +178,47 @@ def sort_cosine_similarities(cosine_similarities):
         # Sort the dictionary by value (item[1])
         # Reverse = True because we want to sort it by decreasing order
         # Documents which are most similar to query are on top
-        sorted_ = sorted(cosine_similarities[query].items(), key=lambda item: item[1], reverse=True)
+        sorted_ = sorted(cosine_similarities[query].items(), key=lambda item: item[1], reverse=True)[:n]
         sorted_cosine_similarities[query] = sorted_
     return sorted_cosine_similarities
 
 
-corpus = read_files()
-corpus = preprocess_data(corpus)
-terms = tokenize_data(corpus)
-terms = eliminate_stopwords(terms)
-terms = stem_tokens(terms)
-terms = eliminate_stopwords(terms)
-terms = eliminate_one_two_character_words(terms)
-inverted_index = build_inverted_index(terms)
-document_weights = cal_document_weights(inverted_index, terms)
-queries = ["Computer Science"]
-queries = preprocess_data(queries)
-query_terms = tokenize_data(queries)
-query_terms = eliminate_stopwords(query_terms)
-query_terms = stem_tokens(query_terms)
-query_terms = eliminate_stopwords(query_terms)
-query_terms = eliminate_one_two_character_words(query_terms)
-query_weights = cal_query_weights(query_terms, inverted_index, terms)
-cosine_similarities = cal_cosine_similarity(query_terms, terms, document_weights, query_weights)
-sorted_cosine_similarities = sort_cosine_similarities(cosine_similarities)
-print(sorted_cosine_similarities)
+def main():
+    corpus = read_files()
+    corpus = preprocess_data(corpus)
+    terms = tokenize_data(corpus)
+    terms = eliminate_stopwords(terms)
+    terms = stem_tokens(terms)
+    terms = eliminate_stopwords(terms)
+    terms = eliminate_one_two_character_words(terms)
+    inverted_index = build_inverted_index(terms)
+    document_weights, weight_squares = cal_document_weights(inverted_index, terms)
 
+    cur_dir = os.path.dirname(__file__)
+    relative_path = '../pickles/terms.pickle'
+    abs_file_path = os.path.join(cur_dir, relative_path)
+
+    with open(abs_file_path, 'wb+') as handle:
+        pickle.dump(terms, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    relative_path = '../pickles/inverted_index.pickle'
+    abs_file_path = os.path.join(cur_dir, relative_path)
+
+    with open(abs_file_path, 'wb+') as handle:
+        pickle.dump(inverted_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    relative_path = '../pickles/document_weights.pickle'
+    abs_file_path = os.path.join(cur_dir, relative_path)
+
+    with open(abs_file_path, 'wb+') as handle:
+        pickle.dump(document_weights, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    relative_path = '../pickles/weight_squares.pickle'
+    abs_file_path = os.path.join(cur_dir, relative_path)
+
+    with open(abs_file_path, 'wb+') as handle:
+        pickle.dump(weight_squares, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == '__main__':
+    main()
